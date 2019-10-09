d07f3f483ca3fc9855c5d14a1c959e32b7912bbf
Kathleen Kamali <kathleenkamali@gmail.com>
2019-10-09 17:24:01 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(words)
   word_array = words.split
   reversed = []
-  word_array.each{|word| reversed << word.reverse!}
+  word_array.collect{|word| reversed << word.reverse!}
   reversed.join(" ")
 end
